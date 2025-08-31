f9774c0dadb23d9bb4caf157e54b9796d49b8728
suffig <philip-melchert@live.de>
2025-08-31 18:34:17 +0200

    build_v4_1

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tr1/sw.js', { scope: '/tr1/' })})}
